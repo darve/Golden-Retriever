851d80ad11933b3fa7246bc9973415d727c516af
David Woollard <dave@darve.co.uk>
2026-01-16 13:48:55 +0000

    removing old clip names

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
 import './App.scss'
 
 import GifCanvas from './components/GifCanvas';
-// import clip from './clips/template';
-// import clip from './clips/overview';
-// import clip from './clips/video-test';
-// import clip from './clips/scene-test';
-// import clip from './clips/save-activities-for-later';
-// import clip from './clips/find-the-perfect-vocabulary';
-// import clip from './clips/text-test';
 import clip from './clips/showcase';
 
 function App() {
